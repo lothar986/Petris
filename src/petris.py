3fe8605f9e59b388f58e4227eb6077c2c0b01a4d
stefanwerleman <stefanwerleman@knights.ucf.edu>
2023-03-08 19:08:39 -0500

    Initialize logger

--- src/petris.py
+++ src/petris.py
@@ -3,6 +3,7 @@
 # Built-in libs
 import os
 import sys
+import logging
 from pathlib import Path
 
 # Third party libs
@@ -13,25 +14,54 @@ import pygame
 sys.path.append(str(Path().parent))
 
 # Custom packages.
+from src import paths
+from src.log.log import initialize_logger
 from src.scenes.scenes import GameMetaData, TitleScene, Scenes
 
-# Positioned Window
-os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (100, 100)
+logger = logging.getLogger(__name__)
 
-#########
-# CLOCK #
-#########
-clock = pygame.time.Clock()
+PETRIS_LOG_FILE = "petris.log"
+PETRIS_LOG_DIR = "logs"
+PETRIS_LOG_PATH = paths.BASE_DIR / PETRIS_LOG_DIR / PETRIS_LOG_FILE
 
-main_screen = pygame.display.set_mode((GameMetaData.screen_width, GameMetaData.screen_height))
-pygame.display.set_caption("Petris")
 
-Scenes.titleScene = TitleScene()
-Scenes.active_scene = Scenes.titleScene
+def main() -> int:
+    
+    exit_code = 0
+    
+    try:
+        initialize_logger(log_path=PETRIS_LOG_PATH)
+        
+        logger.info("Starting Petris Game")
+        
+        # Positioned Window
+        os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (100, 100)
 
-while True:
-    Scenes.active_scene.process_input(pygame.event.get())
-    Scenes.active_scene.update()
-    Scenes.active_scene.render(main_screen)
+        #########
+        # CLOCK #
+        #########
+        clock = pygame.time.Clock()
 
-    clock.tick(50)
+        main_screen = pygame.display.set_mode((GameMetaData.screen_width, GameMetaData.screen_height))
+        pygame.display.set_caption("Petris")
+
+        Scenes.titleScene = TitleScene()
+        Scenes.active_scene = Scenes.titleScene
+
+        while True:
+            Scenes.active_scene.process_input(pygame.event.get())
+            Scenes.active_scene.update()
+            Scenes.active_scene.render(main_screen)
+
+            clock.tick(50)
+    except Exception as ex:
+        exit_code = 1
+        logger.exception(ex)
+    finally:
+        logger.info("End of Petris Game: %s", exit_code)
+        
+    return exit_code
+
+
+if __name__ == "__main__":
+    sys.exit(main())
