4c1c4716d6cf2b89e93030845912a4aad56eb092
stefanwerleman <stefanwerleman@knights.ucf.edu>
2023-03-16 21:22:36 -0400

    Refactor logger

--- src/log/log.py
+++ src/log/log.py
@@ -9,16 +9,17 @@ from pathlib import Path
 class LogFormatter(logging.Formatter):
     """Custom formatter to display colors"""
     
-    _grey = "\x1b[38;20m"
+    _grey = "\033[37m"
+    _white = "\033[39m"
     _yellow = "\x1b[33;20m"
-    _red = "\x1b[31;20m"
+    _red = "\033[91m"
     _bold_red = "\x1b[31;1m"
-    _reset = "\x1b[0m"
+    _reset = "\033[m"
     _format = "%(asctime)s.%(msecs)-3d %(levelname)-8s %(name)s:%(lineno)-10s \t %(message)s"
     
     _formatter_map = {
         logging.DEBUG: _grey + _format + _reset,
-        logging.INFO: _grey + _format + _reset,
+        logging.INFO: _white + _format + _reset,
         logging.WARNING: _yellow + _format + _reset,
         logging.ERROR: _red + _format + _reset,
         logging.CRITICAL: _bold_red + _format + _reset
@@ -30,10 +31,9 @@ class LogFormatter(logging.Formatter):
         return formatter.format(record=record)
 
 
-def initialize_logger(log_path: Path) -> None:
+def initialize_logger(log_path: Path, debug: bool = False) -> None:
     """
     Initializes and configures the logger for the entire application.
-
     Args:
         log_path (Path): Path to logs directory.
     """
@@ -43,7 +43,7 @@ def initialize_logger(log_path: Path) -> None:
     
     
     # Want the root logger to log from the DEBUG level for developers.
-    logging.root.setLevel(logging.DEBUG)
+    logging.root.setLevel(logging.DEBUG if debug else logging.INFO)
     
     # STDOUT for the logger to log to the console
     stream_out_handler = logging.StreamHandler(sys.stdout)
@@ -59,3 +59,4 @@ def initialize_logger(log_path: Path) -> None:
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(formatter)
     logging.root.addHandler(file_handler)
+    
